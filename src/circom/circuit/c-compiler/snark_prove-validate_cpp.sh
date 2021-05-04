0799e3d4ae4b0a26db6cfa65f24edd4628b942bb
timon131 <tiwerb@hotmail.de>
2021-05-04 22:27:43 +0200

    20210504-02

---
+++ src/circom/circuit/c-compiler/snark_prove-validate_cpp.sh
@@ -0,0 +1,13 @@
+#
+#Generate witness and proof; validate the proof (input-specific & circuit-specific)
+#
+
+#calculate the witness and generate its file witness.wtns
+npx snarkit check ../../circuit --backend native --witness_type bin
+
+#generate the proof proof.json (either first: snarkjs | or second: rapidsnark) | generate input_public.json; contains the values of the public inputs and outputs
+#snarkjs groth16 prove circuit_final.zkey witness.wtns proof.json input_public.json
+../../../../rapidsnark/build/prover ../circuit_final.zkey ./data/witness.wtns ../proof.json ./data/output.json
+
+#verify the proof
+time snarkjs groth16 verify ../verification_key.json ./data/output.json ../proof.json
