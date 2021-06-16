9288b5a19fab7ec41960c7a5b81c873c8740984b
timon131 <tiwerb@hotmail.de>
2021-06-16 15:44:44 +0200

    20210616-12

--- generate_ZKP-inputs_v2.js
+++ generate_ZKP-inputs_v2.js
@@ -21,9 +21,9 @@ async function generate_ZKPinputs(l) {
     // define number of features k and sample size n
     let k = 4;
     k++;    //account for X: k+1 x n
-    const n = 500;
+    const n = 250;
     //const n_test = Math.round(n / 2);
-    const n_test = 50;
+    const n_test = 25;
 
     // define accuracy dec (10**dec)
     const dec = 5;
@@ -44,7 +44,7 @@ async function generate_ZKPinputs(l) {
     const require_XX_inv_maxnorm = 10 ** (dec);
     const require_X_trans_Y_maxnorm = k * 10 ** ((2*dec) + 3);
     const require_b_noisy_acc = 2;
-    const require_b_acc = 3;
+    const require_b_acc = 2;
 
     //print sizes of input_public.json
     /*
